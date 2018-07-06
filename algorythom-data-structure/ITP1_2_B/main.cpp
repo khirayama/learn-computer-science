70430e5e4e7a2d5a127a6c0d4c6f651a40b589bd
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-07 08:28:20 +0900

    Pass ITP1_2_B

---
+++ algorythom-data-structure/ITP1_2_B/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+  int c;
+  cin >> a >> b >> c;
+
+  if (a < b && b < c) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
