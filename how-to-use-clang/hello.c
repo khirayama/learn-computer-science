afeb856cc1ae203e317e0f8cfa1bc632e54be35d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-10-20 17:08:51 +0900

    Add types

--- how-to-use-clang/hello.c
+++ how-to-use-clang/hello.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 /* comment */
+/*
+data type
+- int(整数)
+- float(実数)
+- char(1文字)
+*/
 
 int main(void) {
+  int x = 10;
+  float y = 5.2;
+  char c = 'A';
+
+  printf("x = %d, y = %f, c = %c\n", x, y, c);
+
   printf("hello world!\n");
   return 0;
 }
