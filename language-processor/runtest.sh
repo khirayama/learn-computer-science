7c5f2fdf10e6dd5b7082aa3ab289f67e80bd0c21
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-10-20 16:41:37 +0900

    Add test cases

--- language-processor/runtest.sh
+++ language-processor/runtest.sh
@@ -27,9 +27,18 @@ runtest '+ 1 + 2 + 3 4' 10
 runtest '+ 2 *4 3' 14
 
 echo '=== functions ==='
-runtest 'F[+ . .]F(1)' 2
-runtest 'F[* . 2]F(5)' 10
-runtest 'F[* . .]F(F(2))' 16
-runtest 'F[* . .]F(F(F(2)))' 256
+runtest 'F[+ a a]F(1)' 2
+runtest 'F[* a 2]F(5)' 10
+runtest 'F[* a a]F(F(2))' 16
+runtest 'F[* a a]F(F(F(2)))' 256
+runtest 'F[* a a] F(F(F(2)))' 256
+runtest 'F[* a a] F(F(F(F(2))))' 65536
+runtest 'F[* a 3] F(F(F(F(2))))' 162
+runtest 'F[+ a 3] F(F(F(F(2))))' 14
+runtest 'F[+ a 3] F(0)' 3
+runtest 'F[- a 5] G[F(a)] G(0)' -5
+runtest 'F[- a 5] G[F(+ a 5)] G(0)' 0
+runtest 'F[- a 5] G[F(+ a 10)] G(0)' 5
+runtest 'F(* a b) F(3 5)' 15
 
 echo OK
