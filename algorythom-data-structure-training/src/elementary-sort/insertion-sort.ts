a843bd6e24b7e6341c261263240f8804acfec5e6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-02-29 16:24:20 +0900

    Update a test

--- algorythom-data-structure-training/src/elementary-sort/insertion-sort.ts
+++ algorythom-data-structure-training/src/elementary-sort/insertion-sort.ts
@@ -2,10 +2,19 @@
  * 挿入ソート
  *
  ***/
-function add(a: number, b: number): number {
-  return a + b;
+function insertionSort(nums: number[]): number[][] {
+  const result = [[...nums]];
+
+  return result;
 }
 
-test('add', () => {
-  expect(add(1, 3)).toBe(4);
+test('Insertion Sort', () => {
+  expect(insertionSort([5, 2, 4, 6, 1, 3])).toBe([
+    [5, 2, 4, 6, 1, 3],
+    [2, 5, 4, 6, 1, 3],
+    [2, 4, 5, 6, 1, 3],
+    [2, 4, 5, 6, 1, 3],
+    [1, 2, 4, 5, 6, 3],
+    [1, 2, 3, 4, 5, 6],
+  ]);
 });
