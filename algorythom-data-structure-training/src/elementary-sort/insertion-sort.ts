d68307f1cf1cbe6de120e389d4cf278a67397645
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-05 08:02:02 +0900

    Update insertion-sort

--- algorythom-data-structure-training/src/elementary-sort/insertion-sort.ts
+++ algorythom-data-structure-training/src/elementary-sort/insertion-sort.ts
@@ -2,7 +2,7 @@
  * 挿入ソート
  *
  ***/
-import { proc, swap } from '../utils';
+import { proc, swap, assert } from '../utils';
 
 function insertionSort(nums: number[]): number[] {
   insertionSort.proc.reset();
@@ -15,12 +15,12 @@ function insertionSort(nums: number[]): number[] {
       if (num2 === null) {
         break;
       }
+
       console.log(`i: ${i}, j: ${j}, num1: ${num1}, num2: ${num2}, nums: ${nums}`);
 
       if (num2 > num1) {
         console.log('swap', i, j - 1);
         swap(nums, i, j - 1);
-        console.log('swaped', nums);
       } else {
         break;
       }
@@ -29,20 +29,17 @@ function insertionSort(nums: number[]): number[] {
   }
 
   console.log(nums);
-  console.log(insertionSort.proc.state);
   return nums;
 }
 insertionSort.proc = proc;
 
-insertionSort([5, 2, 4, 6, 1, 3]);
-// test('Insertion Sort', () => {
-//   expect(insertionSort([5, 2, 4, 6, 1, 3])).toBe([1, 2, 3, 4, 5, 6]);
-//   // expect(insertionSort.proc.state).toBe([
-//   //   [5, 2, 4, 6, 1, 3],
-//   //   [2, 5, 4, 6, 1, 3],
-//   //   [2, 4, 5, 6, 1, 3],
-//   //   [2, 4, 5, 6, 1, 3],
-//   //   [1, 2, 4, 5, 6, 3],
-//   //   [1, 2, 3, 4, 5, 6],
-//   // ]);
-// });
+// insertionSort([5, 2, 4, 6, 1, 3]);
+assert(insertionSort([5, 2, 4, 6, 1, 3]), [1, 2, 3, 4, 5, 6]);
+assert(insertionSort.proc.state, [
+  [5, 2, 4, 6, 1, 3],
+  [2, 5, 4, 6, 1, 3],
+  [2, 4, 5, 6, 1, 3],
+  [2, 4, 5, 6, 1, 3],
+  [1, 2, 4, 5, 6, 3],
+  [1, 2, 3, 4, 5, 6],
+]);
