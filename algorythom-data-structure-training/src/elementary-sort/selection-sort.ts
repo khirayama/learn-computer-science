5b2eadef07ac22ada41d49d4e1824e8191bd514f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-15 19:36:05 +0900

    Update comments

--- algorythom-data-structure-training/src/elementary-sort/selection-sort.ts
+++ algorythom-data-structure-training/src/elementary-sort/selection-sort.ts
@@ -2,6 +2,8 @@
  * [選択ソート - Wikipedia](https://ja.wikipedia.org/wiki/%E9%81%B8%E6%8A%9E%E3%82%BD%E3%83%BC%E3%83%88)
  * 配列された要素から、最大値やまたは最小値を探索し配列最後の要素と入れ替えをおこなうこと。最悪計算時間がO(n2)と遅いが、アルゴリズムが単純で実装が容易なため、しばしば用いられる。内部ソート。後述するように、安定ソートではない。
  *
+ * 最悪計算時間がO(n2)と遅いが、アルゴリズムが単純で実装が容易なため、しばしば用いられる。
+ *
  * 最悪計算時間: O(n2)
  * 最良計算時間: O(n2)
  * 平均計算時間: O(n2)
@@ -11,7 +13,7 @@
 import { proc, assert } from '../utils';
 
 function selectionSort(nums: number[]): number[] {
-  shellSort.proc.reset();
+  selectionSort.proc.reset();
   selectionSort.proc.log(nums);
 
   for (let i = 0; i < nums.length; i += 1) {
