3c3d805bc4782c8eaf2b0ff87ffa0b6013f4fda5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-23 07:50:32 +0900

    Update lexer

--- go-monkey/src/monkey/lexer/lexer.go
+++ go-monkey/src/monkey/lexer/lexer.go
@@ -1,6 +1,8 @@
+// Unicode/UTF-8をサポートするならruneの必要がある。P7
+
 package lexer
 
-// Unicode/UTF-8をサポートするならruneの必要がある。P7
+import "monkey/token"
 
 type Lexer struct {
 	input        string
@@ -23,4 +25,36 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
