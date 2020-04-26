4c3043d7373806e73c5be02dbfaf296486708c59
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-26 09:34:32 +0900

    Create parser_test

---
+++ go-monkey/src/monkey/parser/parser_test.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"testing"
+
+	"monkey/ast"
+	"monkey/lexer"
+)
+
+func TestLestStatement(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	return true
+}
