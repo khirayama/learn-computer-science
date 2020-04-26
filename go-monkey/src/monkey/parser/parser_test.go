99dbb7304e3a4397742282439d0b2a371b600584
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-26 10:37:28 +0900

    Update test case

--- go-monkey/src/monkey/parser/parser_test.go
+++ go-monkey/src/monkey/parser/parser_test.go
@@ -41,5 +41,26 @@ let foobar = 838383;
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
 	return true
 }
