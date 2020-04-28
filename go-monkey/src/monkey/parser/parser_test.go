f33f5e273f7436e0b88a4468905dae2614d9a014
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-28 16:49:24 +0900

    Add checkParserErrors

--- go-monkey/src/monkey/parser/parser_test.go
+++ go-monkey/src/monkey/parser/parser_test.go
@@ -64,3 +64,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 
 	return true
 }
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
