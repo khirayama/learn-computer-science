359f375920db515b10730016245083fdbc72319b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-28 16:45:49 +0900

    Implement Erros of Parser

--- go-monkey/src/monkey/parser/parser.go
+++ go-monkey/src/monkey/parser/parser.go
@@ -96,6 +96,10 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	}
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
 func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
