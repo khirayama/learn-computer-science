d72876d26864abb4b902116131ba352864c9ee58
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-08-14 22:25:16 +0900

    Learn standard io

--- learn-new-langs/playground/Main.cpp
+++ learn-new-langs/playground/Main.cpp
@@ -5,6 +5,15 @@
 using namespace std;
 
 int main() {
-	cout << "Kitty on your lap";
+  // [基本入出力](http://wisdom.sakura.ne.jp/programming/cpp/cpp2.html)
+  char str[128];
+  cout << "名前を入力してください>";
+  cin >> str;
+  cout << "ようこそ " << str << " さん";
+
+  int i , j;
+  cout << "二つの値を加算します>";
+  cin >> i >> j;
+	cout << i + j;
 	return 0;
 }
