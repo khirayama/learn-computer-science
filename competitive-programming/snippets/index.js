1c96e5c9eaaf2767f7ccd5b91b8469f989daa5e4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-09-02 07:03:36 +0900

    Update snippets

--- competitive-programming/snippets/index.js
+++ competitive-programming/snippets/index.js
@@ -1,5 +1,6 @@
 /*
  * Binary Search
+ * Priority Queue
  */
 
 // Binary Search
@@ -16,3 +17,33 @@ while (left < right) {
   mid = Math.floor((left + right) / 2);
 }
 return mid;
+
+// Priority Queue
+class PriorityQueueForKSmallestPairs {
+  constructor() {
+    this.items = [];
+  }
+  
+  enqueue(item/* { pos1, pos2, num1, num2 } */) {
+    let left = 0;
+    let right = this.items.length;
+    let mid = Math.floor((left + right) / 2);
+
+    while (left < right && this.items.length !== 0) {
+      const tmp = this.items[mid];
+
+      if (tmp.num1 + tmp.num2 <= item.num1 + item.num2) {
+        mid += 1;
+        left = mid;
+      } else {
+        right = mid;
+      }
+      mid = Math.floor((left + right) / 2);
+    }
+    this.items.splice(mid, 0, item);
+  }
+  
+  dequeue() {
+    return this.items.shift();
+  }
+}
