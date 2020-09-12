2eb22ba08c7ab53e08465424315ef5f0fab27662
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-09-12 14:48:10 +0900

    Update script

--- practices/slam/catkin_ws/build/catkin_generated/setup_cached.sh
+++ practices/slam/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/root/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/root/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/root/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/root/catkin_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/root/catkin_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/root/catkin_ws/src:$ROS_PACKAGE_PATH"
