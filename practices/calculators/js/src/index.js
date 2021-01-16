90b6c19437240930e2749de9b3c473b46cbde75b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-16 15:57:46 +0900

    Create calculator

---
+++ practices/calculators/js/src/index.js
@@ -0,0 +1,114 @@
+// http://jorendorff.github.io/calc/docs/calculator-backends.html
+const assert = require('assert');
+
+function tokenize(code) {
+  const results = [];
+  const tokenRegExp = /\s*([A-Za-z]+|[-1-9]+|\S)\s*/g;
+
+  let m = tokenRegExp.exec(code);
+  while (m !== null) {
+    results.push(m[1]);
+    m = tokenRegExp.exec(code);
+  }
+  return results;
+}
+
+function isNumeric(token) {
+  return token !== undefined && token.match(/^[0-9]+$/) !== null;
+}
+
+function isName(token) {
+  return token !== undefined && token.match(/^[A-Za-z]+$/) !== null;
+}
+
+function parse(code) {
+  const tokens = tokenize(code);
+  let position = 0;
+
+  function peek() {
+    return tokens[position];
+  }
+
+  function consume(token) {
+    assert.strictEqual(token, tokens[position]);
+    position++;
+  }
+
+  function parsePrimaryExpr() {
+    const t = peek();
+
+    if (isNumeric(t)) {
+      consume(t);
+      return { type: 'number', value: t };
+    } else if (isName(t)) {
+      consume(t);
+      return { type: 'name', id: t };
+    } else if (t === '(') {
+      consume(t);
+      const expr = parseExpr();
+      if (peek() !== ')') throw new SyntaxError('expected )');
+      consume(')');
+      return expr;
+    } else {
+      throw new SyntaxError('expected a number, a variable, or parentheses');
+    }
+  }
+
+  function parseMulExpr() {
+    let expr = parsePrimaryExpr();
+    let t = peek();
+    while (t === '*' || t === '/') {
+      consume(t);
+      const rhs = parsePrimaryExpr();
+      expr = { type: t, left: expr, right: rhs };
+      t = peek();
+    }
+    return expr;
+  }
+
+  function parseExpr() {
+    let expr = parseMulExpr();
+    let t = peek();
+    while (t === '+' || t === '-') {
+      consume(t);
+      const rhs = parseMulExpr();
+      expr = { type: t, left: expr, right: rhs };
+      t = peek();
+    }
+    return expr;
+  }
+
+  const result = parseExpr();
+
+  if (position !== tokens.length) {
+    throw new SyntaxError("unexpected '" + peek() + "'");
+  }
+
+  return result;
+}
+
+function evaluate(code) {
+  const variables = Object.create(null);
+  variables.e = Math.E;
+  variables.pi = Math.PI;
+
+  function evaluate(obj) {
+    switch (obj.type) {
+      case 'number':
+        return parseInt(obj.value);
+      case 'name':
+        return variables[obj.id] || 0;
+      case '+':
+        return evaluate(obj.left) + evaluate(obj.right);
+      case '-':
+        return evaluate(obj.left) - evaluate(obj.right);
+      case '*':
+        return evaluate(obj.left) * evaluate(obj.right);
+      case '/':
+        return evaluate(obj.left) / evaluate(obj.right);
+    }
+  }
+  return evaluate(parse(code));
+}
+
+module.exports = { tokenize, isNumeric, isName, parse, evaluate };
---
+++ practices/calculators/js/src/index.spec.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+
+const { tokenize, isNumeric, isName, parse, evaluate } = require('./index');
+
+describe('tokenize', () => {
+  it('work correctly', () => {
+    assert.deepEqual(tokenize('123\n'), ['123']);
+    assert.deepEqual(tokenize('2+2'), ['2', '+', '2']);
+    assert.deepEqual(tokenize('+-*/'), ['+', '-', '*', '/']);
+    assert.deepEqual(tokenize('   1   * 24 +\n\n  pi'), ['1', '*', '24', '+', 'pi']);
+    assert.deepEqual(tokenize('()'), ['(', ')']);
+    assert.deepEqual(tokenize('    '), []);
+  });
+});
+
+describe('isNumeric', () => {
+  it('work correctly', () => {
+    assert(isNumeric('123'));
+    assert(!isNumeric('x'));
+    assert(!isNumeric('-'));
+  });
+});
+
+describe('isName', () => {
+  it('work correctly', () => {
+    assert(isName('xyz'));
+    assert(!isName('+'));
+  });
+});
+
+describe('parse', () => {
+  it('work correctly', () => {
+    assert.deepEqual(parse('(1 + 2) / 3'), {
+      type: '/',
+      left: {
+        type: '+',
+        left: { type: 'number', value: '1' },
+        right: { type: 'number', value: '2' },
+      },
+      right: { type: 'number', value: '3' },
+    });
+  });
+});
+
+describe('evaluate', () => {
+  it('work correctly', () => {
+    assert.strictEqual(evaluate('2 + 2'), 4);
+    assert.strictEqual(evaluate('3 * 4 * 5'), 60);
+    assert.strictEqual(evaluate('5 * (2 + 2)'), 20);
+  });
+});
