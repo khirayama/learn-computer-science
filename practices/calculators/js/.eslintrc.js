7ce0242f80814a91d1891cdca25bfca693aff54c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-16 15:51:30 +0900

    Change eslint config

--- practices/calculators/js/.eslintrc.js
+++ practices/calculators/js/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:node/recommended', 'plugin:prettier/recommended', 'google'],
+  extends: ['plugin:node/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
