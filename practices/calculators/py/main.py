da30e8b2092f735e9137afab80f1dbff25141129
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-23 08:54:50 +0900

    Format python files

--- practices/calculators/py/main.py
+++ practices/calculators/py/main.py
@@ -1,14 +1,14 @@
 import re
 
 def tokenize(code):
-    results = []
     token_regexp = r'\s*([A-Za-z]+|[-1-9]+|\S)\s*'
-    re_pattern = re.compile(token_regexp)
 
     return re.findall(token_regexp, code, re.S)
 
+
 def is_numeric(token):
     return re.match(r'^[0-9]+$', token)
+
 
 def is_name(token):
     return re.match(r'^[A-Za-z]+$', token)
--- practices/calculators/py/test.py
+++ practices/calculators/py/test.py
@@ -2,15 +2,19 @@ import unittest
 
 import main
 
+
 class TestCalculator(unittest.TestCase):
 
     def test_tokenize(self):
-        self.assertEqual(main.tokenize('123\n'), ['123']);
-        self.assertEqual(main.tokenize('2+2'), ['2', '+', '2']);
-        self.assertEqual(main.tokenize('+-*/'), ['+', '-', '*', '/']);
-        self.assertEqual(main.tokenize('   1   * 24 +\n\n  pi'), ['1', '*', '24', '+', 'pi']);
-        self.assertEqual(main.tokenize('()'), ['(', ')']);
-        self.assertEqual(main.tokenize('     '), []);
+        self.assertEqual(main.tokenize('123\n'), ['123'])
+        self.assertEqual(main.tokenize('2+2'), ['2', '+', '2'])
+        self.assertEqual(main.tokenize('+-*/'), ['+', '-', '*', '/'])
+        self.assertEqual(
+                main.tokenize('   1   * 24 +\n\n  pi'),
+                ['1', '*', '24', '+', 'pi']
+                )
+        self.assertEqual(main.tokenize('()'), ['(', ')'])
+        self.assertEqual(main.tokenize('     '), [])
 
     def test_is_numeric(self):
         self.assertTrue(main.is_numeric('123'))
